6882e1ef59ed9c0064aafce15a518d9f5a58ef49
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-11-01 19:02:35 +0700

    enhance(chore): move config into public folder

---
+++ backend/public/config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
+if(!isset($_ENV['DB_HOST'])){
+    die('.env not loaded');
+}
---
+++ backend/public/config/database.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+
+class Database {
+    private static ?PDO $instance = null;
+
+    public static function getConnection(): PDO {
+        if(self::$instance === null){
+            $host = $_ENV['DB_HOST'] ?: '127.0.0.1';
+            $db = $_ENV['DB_NAME'] ?: '';
+            $user = $_ENV['DB_USER'] ?: '';
+            $pass = $_ENV['DB_PASS'] ?: '';
+            $charset = 'utf8mb4';
+
+            
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
+
+            self::$instance = new PDO($dsn, $user, $pass, $options);
+
+        }
+        return self::$instance;
+    }
+}
